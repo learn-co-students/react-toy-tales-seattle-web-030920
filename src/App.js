dce59121798a31314d4afa5cb2a81a92f00cece2
christine <bumpouce@gmail.com>
2020-05-18 13:26:45 -0700

    Done.

--- src/App.js
+++ src/App.js
@@ -11,7 +11,9 @@ import data from './data'
 class App extends React.Component{
 
   state = {
-    display: false
+    display: false,
+    toyData: data,
+    nextID: data.length + 1
   }
 
   handleClick = () => {
@@ -21,20 +23,53 @@ class App extends React.Component{
     })
   }
 
+  likeToy = (thisToy) => {
+    console.log(`Like Toy: ${thisToy.name}`)
+    thisToy.likes += 1
+
+    this.setState(previousState => { 
+      return {toyData: previousState.toyData.map(toy => {
+        if (toy.id === thisToy.id) { return thisToy}
+        else {return toy}
+      })}
+    })
+  }
+
+  donateToy = (thisToy) => {
+    console.log(`Donating Toy: ${thisToy.name}`)
+    this.setState(previousState => {
+      return {toyData: previousState.toyData.filter(toy => {
+        if (toy.id !== thisToy.id) {return toy}
+      })}
+    })
+  }
+
+  createToy = (formInput) => {
+    formInput.preventDefault()
+
+    const {name, image} = formInput.target
+    const newToy = {id:this.state.nextID, image:image.value, likes: 0, name: name.value}
+
+    this.setState({toyData: [...this.state.toyData, newToy], nextID: this.state.nextID + 1})
+
+    alert(`Added ${newToy.name}!`)
+    formInput.target.reset()
+  }
+
   render(){
     return (
       <>
         <Header/>
         { this.state.display
             ?
-          <ToyForm/>
+          <ToyForm onAddToy={this.createToy}/>
             :
           null
         }
         <div className="buttonContainer">
-          <button onClick={this.handleClick}> Add a Toy </button>
+          <button onClick={this.handleClick}>{this.state.display? "Close" : "Open"} New Toy Form </button>
         </div>
-        <ToyContainer/>
+        <ToyContainer toyData={this.state.toyData} onLikeToy={this.likeToy} onDonateToy={this.donateToy}/>
       </>
     );
   }
--- src/components/ToyForm.jsx
+++ src/components/ToyForm.jsx
@@ -5,11 +5,11 @@ class ToyForm extends Component {
   render() {
     return (
       <div className="container">
-        <form className="add-toy-form">
+        <form className="add-toy-form" onSubmit={this.props.onAddToy}>
           <h3>Create a toy!</h3>
-          <input type="text" name="name" placeholder="Enter a toy's name..." className="input-text"/>
+          <input type="text" name="name" placeholder="Enter a toy's name..." className="input-text" />
           <br/>
-          <input type="text" name="image" placeholder="Enter a toy's image URL..." className="input-text"/>
+          <input type="text" name="image" placeholder="Enter a toy's image URL..." className="input-text" />
           <br/>
           <input type="submit" name="submit" value="Create New Toy" className="submit"/>
         </form>
